59ddea45071e9be1ead35b2606ea7322092aa45e
sour-soup <kiziyepi8@gmail.com>
2025-01-23 18:34:18 +0400

    feat(task1): initial implementation (high jitter)

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,22 @@
-void setup() {
+const int N = 5;
+const int pins[] = { 3, 5, 6, 9, 10 };
+const unsigned long pulseDuration[] = { 10000, 1000, 500, 100, 50 };
+unsigned long lastToggleTime[N];
+bool pinState[N];
 
+
+void setup() {
+  for (int i = 0; i < N; ++i)
+    pinMode(pins[i], OUTPUT);
 }
 
 void loop() {
-
+  for (int i = 0; i < N; ++i) {
+    unsigned long currentTime = micros();
+    if (2 * (currentTime - lastToggleTime[i]) >= pulseDuration[i]) {
+      pinState[i] ^= 1;
+      digitalWrite(pins[i], pinState[i]);
+      lastToggleTime[i] = currentTime;
+    }
+  }
 }
